af5fd400884946fa8d574d05e8a57711ba7e46e1
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-06-21 17:42:52 -0400

    set up server.js file

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// tells mongoose database what to connect to
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
+    userNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`You did it! Connected on localhost:${PORT}`));
