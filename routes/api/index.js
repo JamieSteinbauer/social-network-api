367dc02adc36fe3cc51eb98d893639b5d616157a
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-06-23 19:40:24 -0400

    added routes to index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 // import the api routes to prefix their endpoint names
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
 // add prefix of '/user' to routes created in 'user-routes.js'
 router.use('/user', userRoutes);
+// same thought process for thoughts
+router.use('/thought', thoughtRoutes);
 
 module.exports = router;
